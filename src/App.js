39be7544441933900247077910836123bc226433
Breeze Artist <Breezartist29@gmail.com>
2023-04-24 16:14:05 +0700

    fn:hearder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-
-import './App.scss';
+import "./App.scss";
+import { FaHome } from "react-icons/fa";
 
 function App() {
   return (
-  <div className='container'>
-    {/* header */}
-    <header className='header'>header</header>
-    {/* sidebar */}
-    <aside className='sidebar'>sidebar</aside>
-    {/* content */}
-    <main className='content'>content</main>
-  </div>
+    <div className="container">
+      {/* header */}
+      <header className="header">
+        <span>
+          <FaHome />
+        </span>
+        <h3>todolist</h3>
+      </header>
+      {/* sidebar */}
+      <aside className="sidebar">sidebar</aside>
+      {/* content */}
+      <main className="content">content</main>
+    </div>
   );
 }
 
